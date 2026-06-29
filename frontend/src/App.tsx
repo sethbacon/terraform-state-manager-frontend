09ca9e06a0743d3a8fce60c6ec7bf001a6563703
Seth Bacon <stbacon@gmail.com>
2026-06-29 06:34:03 -0600

    feat(devtools): add React Query Devtools for parity with registry

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { lazy, type ReactNode } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { AppThemeProvider } from './contexts/ThemeContext'
 import { AuthProvider } from './contexts/AuthContext'
@@ -117,6 +118,7 @@ export default function App() {
           <ConsentBanner />
         </ConsentProvider>
       </AppThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
